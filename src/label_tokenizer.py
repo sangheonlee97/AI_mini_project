0e5dfd0446a832c5142dd9ffe774b7daf20e1573
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-02-23 15:55:12 +0900

    단어사전 딕셔너리를 json 파일로 저장

--- src/label_tokenizer.py
+++ src/label_tokenizer.py
@@ -1,6 +1,7 @@
 from keras.preprocessing.text import Tokenizer
 import numpy as np
 import pandas as pd
+import json
 
 df = pd.read_csv('../resource/word.csv')
 print(df)
@@ -17,3 +18,12 @@ df['한국어'] = label_real
 print(df)
 
 df.to_csv('../resource/tokend_word.csv', index=0)
+
+####################
+dfdf = token.word_index                               ### 딕셔너리 저장
+with open('token_dic.json', 'w') as json_file:
+    json.dump(dfdf, json_file)
+    
+# with open('token_dic.json', 'r') as json_file:          ### 딕셔너리 불러오기
+#     dfdf = json.load(json_file)
+# print(dfdf)
